60f36077de5493ae3935d4d40d3712be84ca7971
Anil <anil_gurung15@yahoo.com>
2020-03-28 13:33:00 -0700

    changed indices back to -1 from 0 in Detail class

--- detail.rb
+++ detail.rb
@@ -23,22 +23,22 @@ class Detail
 
     def movie_link
         @@link[:link] << "https://www.hulu.com"+@@doc1.css('div.jsx-1977775403.NonSubSimpleCollection.cu-non-sub-simple-collection')[@category_index].css('a.Tile__title-link')[@movie_index].attributes['href'].value
-        puts "Movie Link : #{@@link[:link][0]}"
+        puts "Movie Link : #{@@link[:link][-1]}"
     end
 
     def movie_title
         @@link[:title] << Nokogiri::HTML(open(@@link[:link][-1])).css('h1.DetailEntityMasthead__title').text
-        puts "Movie Title : #{@@link[:title][0]}"
+        puts "Movie Title : #{@@link[:title][-1]}"
     end
 
     def movie_genre
         @@link[:genre] << Nokogiri::HTML(open(@@link[:link][-1])).css('div.DetailEntityMasthead__tags').text
-        puts "Movie Genre : #{@@link[:genre][0]}"
+        puts "Movie Genre : #{@@link[:genre][-1]}"
     end
 
     def movie_description
         @@link[:description] << Nokogiri::HTML(open(@@link[:link][-1])).css('p.jsx-1677985992.DetailEntityModal__description').text
-        puts "Movie Description : #{@@link[:description][0]}"
+        puts "Movie Description : #{@@link[:description][-1]}"
     end
 
 end
